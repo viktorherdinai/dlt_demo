685e7c6b8b18498e8e14df9971b6d18d75673436
Viktor Herdinai <viktor.herdinai@lhsystems.com>
2024-07-18 14:00:11 +0200

    added new fields to schema

--- dlt_demo/utils/create_mock_data.py
+++ dlt_demo/utils/create_mock_data.py
@@ -38,9 +38,11 @@ def create_worker(*, new_field: bool = False) -> str:
         "worker_name": person.name,
         "salary": random.randint(1000, 10000),
         "bonus": random.randint(0, 1000),
-        "address": random.choice(addresses)
+        "address": random.choice(addresses),
+        "created_at": int(time.time()),
+        "gender": random.choice(["M", "F", "O"]),
     }
-    if new_field and random.random() <= 0.01:
+    if new_field and random.random() <= 0.1:
         worker |= {"position": random.choice(position)}
 
     return json.dumps(worker).encode("utf-8")
@@ -64,12 +66,14 @@ def stream_mock_data(*, delay: int) -> None:
     Args:
         delay (int): Delay between each upload in seconds.
     """
+    uploaded_files = 0
     while True:
         worker_obj = create_file_like_object(create_worker(new_field=True))
         upload_to_bucket(worker_obj, "databricks-dlt-demo-dev", f"mock_data/worker-{int(time.time())}.json")
-        logger.info(f"Uploaded mock data to S3.")
+        uploaded_files += 1
+        logger.info(f"[{uploaded_files}] - Uploaded mock data to S3.")
         time.sleep(delay)
 
 
 if __name__ == '__main__':
-    stream_mock_data(delay=10)
+    stream_mock_data(delay=1)
