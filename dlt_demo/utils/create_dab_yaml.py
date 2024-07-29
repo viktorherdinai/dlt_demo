555fd6cc678323ed80003c7fe7ed0ef807e22ea1
Viktor Herdinai <viktor.herdinai@lhsystems.com>
2024-07-29 10:23:57 +0200

    added logger info comments

--- dlt_demo/utils/create_dab_yaml.py
+++ dlt_demo/utils/create_dab_yaml.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 from dotenv import load_dotenv
@@ -6,15 +7,19 @@ from pathlib import Path
 
 load_dotenv()
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(name)s [%(levelname)s] %(message)s")
 repo_root = Path(__file__).parents[2]
 
 
 def main():
     with open(repo_root / "templates" / 'databricks.yaml.j2') as f:
         template = Template(f.read()).render(env=os.environ)
+    logger.info("Databricks template file read and rendered successfully.")
 
     with open(repo_root / 'databricks.yaml', 'w') as f:
         f.write(template)
+    logger.info("Databricks YAML file created successfully.")
 
 
 if __name__ == '__main__':
